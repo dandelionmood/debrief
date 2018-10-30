9c401b783f14fc8d3d2de125345262c348727c59
Pierre Quillery <p.quillery@sdv.fr>
2018-10-30 22:56:30 +0100

    fix(sass) : repaired broken sass compilation

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,16 +17,23 @@ require('dotenv').config();
 mix
     // JS compilation
     .js('resources/assets/js/app.js', 'public/js')
-        .sourceMaps() // we add sourcemaps to ease debug
-        // .browserSync('localhost:8000') // we allow for automatic browser refresh
-        .extract([
-            'jquery', 'bootstrap', 'lodash', 'axios',
-            'moment', 'bootstrap-datepicker',
-            'textcomplete', 'dropzone',
-            'jstree'
-        ])
+    .sourceMaps() // we add sourcemaps to ease debug
+    // .browserSync('localhost:8000') // we allow for automatic browser refresh
+    .extract([
+        'jquery', 'bootstrap', 'lodash', 'axios',
+        'moment', 'bootstrap-datepicker',
+        'textcomplete', 'dropzone',
+        'jstree'
+    ])
     // CSS compilation
     .sass('resources/assets/sass/app.scss', 'public/css', {
         // Theme colors are read from the .env file, in a «slack» fashion.
         'data': "$mix-theme-colors: '" + process.env.MIX_THEME_COLORS + "';"
+    })
+    .webpackConfig({ // not in use right now but we might need it at some point
+        resolve: {
+            alias: {
+                // './fonts/titillium': path.resolve(__dirname, 'public/fonts/vendor/jstree-bootstrap-theme/dist/themes/proton/titillium')
+            }
+        }
     });
