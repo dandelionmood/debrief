1aa14c9180b493a4ca5e278465fb4a878f078ef3
Pierre Quillery <pierre@quillery.fr>
2018-11-15 23:06:08 +0100

    feat(universes) : better buttons

--- resources/views/universes/show.blade.php
+++ resources/views/universes/show.blade.php
@@ -4,31 +4,25 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-10">
-            <h1>
-                @include('shared.edit-in-place', [
-                    'model' => $universe,
-                    'field' => 'label',
-                    'field_type' => 'text',
-                    'route' => ['universes.update', $universe->id],
-                ])
-            </h1>
-        </div>
-        <div class="col-1" style="text-align: right;">
-            {!! Form::model($universe, ['route' => ['universes.edit', $universe->id], 'method' => 'GET']) !!}
-            <button class="btn">
-                <span class="oi oi-pencil" title="edit" aria-hidden="true"></span>
-            </button>
-            {!! Form::close() !!}
-        </div>
-        <div class="col-1" style="text-align: right;">
-            {!! Form::model($universe, ['route' => ['universes.destroy', $universe->id], 'method' => 'DELETE', 'class' => 'form-inline']) !!}
-            <button class="btn">
-                <span class="oi oi-trash" title="trash" aria-hidden="true"></span>
-            </button>
-            {!! Form::close() !!}
-        </div>
+    <div class="d-flex">
+        <h1 class="flex-grow-1">
+            @include('shared.edit-in-place', [
+                'model' => $universe,
+                'field' => 'label',
+                'field_type' => 'text',
+                'route' => ['universes.update', $universe->id],
+            ])
+        </h1>
+        {!! Form::model($universe, ['route' => ['universes.edit', $universe->id], 'method' => 'GET']) !!}
+        <button class="btn" role="button">
+            <span class="oi oi-pencil" title="edit" aria-hidden="true"></span>
+        </button>
+        {!! Form::close() !!}
+        {!! Form::model($universe, ['route' => ['universes.destroy', $universe->id], 'method' => 'DELETE']) !!}
+        <button class="btn" role="button">
+            <span class="oi oi-trash" title="trash" aria-hidden="true"></span>
+        </button>
+        {!! Form::close() !!}
     </div>
 
     @include('shared.edit-in-place', [
@@ -37,10 +31,6 @@
         'field_type' => 'textarea',
         'route' => ['universes.update', $universe->id],
     ])
-
-    <hr/>
-
-    <a href="{{ route('universes.stories.index', $universe->id) }}">See all the related stories…</a>
 
 @endsection
 
